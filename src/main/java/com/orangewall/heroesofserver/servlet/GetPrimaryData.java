9e30658a41858ccd4bc0673622c6d4b4f1f11ceb
Daniel Franco Lopes <dandanielfranco@yahoo.com.br>
2019-10-29 14:12:13 -0300

    Add userId submission handling on GetPrimaryData

--- src/main/java/com/orangewall/heroesofserver/servlet/GetPrimaryData.java
+++ src/main/java/com/orangewall/heroesofserver/servlet/GetPrimaryData.java
@@ -9,6 +9,10 @@ import com.orangewall.heroesofserver.model.*;
 import com.orangewall.heroesofserver.util.JSON;
 import com.orangewall.heroesofserver.util.SQL;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,7 +21,18 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/getPrimaryData")
 public class GetPrimaryData extends HttpServlet {
-
+    
+    List<Class<?>> entityClasses;
+    
+    @Override
+    public void init() throws ServletException {
+        entityClasses = new ArrayList<>(
+            Arrays.asList(
+                AssetFilter.class, Score.class, Part.class, Phase.class,
+                GameUser.class, Troop.class, Barrack.class, Tower.class,
+                Skill.class, Hero.class, Speak.class, Scene.class, Cutscene.class));
+    }
+    
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
@@ -25,24 +40,41 @@ public class GetPrimaryData extends HttpServlet {
         resp.setCharacterEncoding("UTF-8");
         req.setCharacterEncoding("UTF-8");
         
-        Class<?>[] entityClasses = {
-            AssetFilter.class, Score.class, Part.class, Phase.class,
-            GameUser.class, Troop.class, Barrack.class, Tower.class, Skill.class,
-            Hero.class, Speak.class, Scene.class, Cutscene.class
-        };
         JsonParser parser = new JsonParser();
         JsonObject jsonResponse = new JsonObject();
+        String userId = req.getParameter("userId");
         
         for (Class<?> entityClass : entityClasses) {
             try {
+                List<?> records,
+                        primaryKeys = SQL.getPrimaryKeysNames(entityClass);
+                
+                if (entityClass.equals(Score.class)) {                    
+                    if (userId == null || userId.isEmpty()) {
+                        continue;                        
+                    }                    
+                    records = ((List<Score>) SQL.getRegistros(entityClass)).stream()
+                            .filter(score -> score.getIdtGoogleAccount().equals(userId))
+                            .collect(Collectors.toList());
+                } else if (entityClass.equals(GameUser.class)) {                    
+                    if (userId == null || userId.isEmpty()) {
+                        continue;                        
+                    }                    
+                    records = ((List<GameUser>) SQL.getRegistros(entityClass)).stream()
+                            .filter(user -> user.getIdtGoogleAccount().equals(userId))
+                            .collect(Collectors.toList());
+                } else {
+                    records = SQL.getRegistros(entityClass);
+                }
+                
                 JsonObject table = new JsonObject();
-                JsonArray records = parser.parse(JSON.stringify(SQL
-                        .getRegistros(entityClass))).getAsJsonArray(),
-                          primaryKeys = parser.parse(JSON.stringify(SQL
-                        .getPrimaryKeysNames(entityClass))).getAsJsonArray();
+                JsonArray jsonRecords = parser.parse(
+                        JSON.stringify(records)).getAsJsonArray(),
+                          jsonPrimaryKeys = parser.parse(
+                        JSON.stringify(primaryKeys)).getAsJsonArray();
 
-                table.add("primaryKeys", primaryKeys);
-                table.add("records", records);
+                table.add("primaryKeys", jsonPrimaryKeys);
+                table.add("records", jsonRecords);
                 
                 jsonResponse.add(SQL.getNomeTabela(entityClass), table);
             } catch (NotTableException ex) {
